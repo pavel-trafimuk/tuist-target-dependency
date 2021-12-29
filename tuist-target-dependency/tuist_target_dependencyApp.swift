8a2bb5f7466f6f798d1d7bcbb58ef1a240042a08
Pavel Trafimuk <pavel@viber.com>
2021-12-29 10:53:19 +0300

    Initial Commit

---
+++ tuist-target-dependency/tuist_target_dependencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  tuist_target_dependencyApp.swift
+//  tuist-target-dependency
+//
+//  Created by Paweł Trafimuk on 29/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct tuist_target_dependencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
